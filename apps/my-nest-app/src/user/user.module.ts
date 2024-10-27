9b7f0efc2f900032dc50fa1184897a9554cb5dff
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-10-27 18:29:55 +1100

    add pubsub

--- apps/my-nest-app/src/user/user.module.ts
+++ apps/my-nest-app/src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MssqlModule } from '@poc-gcp/mssql';
+import { PubsubModule } from '@poc-gcp/pubsub';
 
 @Module({
   imports: [
-    MssqlModule
+    MssqlModule,
+    PubsubModule
   ],
   controllers: [UserController],
   providers: [UserService],
