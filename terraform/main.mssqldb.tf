62a9e42822fa529f9c8994a0c11e9a1077634dd7
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-10-27 17:56:59 +1100

    add sql

---
+++ terraform/main.mssqldb.tf
@@ -0,0 +1,18 @@
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  region           = var.gcp_region
+  database_version = "SQLSERVER_2017_EXPRESS"
+  root_password    = var.gcp_mssql_pw
+
+  settings {
+    tier = "db-custom-1-6000"
+  }
+
+  deletion_protection = "false"
+}
