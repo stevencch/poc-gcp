dea7b7aeaa194d6d2debb4233ae41a09b981aaf7
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-11-01 18:10:30 +1100

    update

--- apps/myapp1/src/app/app.service.ts
+++ apps/myapp1/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello APIx666' };
+    return { message: 'Hello APIx777' };
   }
 }
