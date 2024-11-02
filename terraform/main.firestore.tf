1e4c00d90898728cbb92d9bdbfaf5cca262d5caf
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-11-02 20:25:32 +1100

    add rule

--- terraform/main.firestore.tf
+++ terraform/main.firestore.tf
@@ -17,3 +17,16 @@ resource "google_firestore_field" "firestore_outbox_expire_at" {
   ttl_config {}
   index_config {}
 }
+
+
+# Define the security ruleset for Firestore
+resource "google_firebaserules_ruleset" "firestore" {
+  project = var.gcp_project_id
+  source {
+    files {
+      content = "service cloud.firestore {match /databases/{database}/documents { match /{document=**} { allow read, write: if true; } } }"
+      name    = "firestore.rules"
+    }
+  }
+}
+
