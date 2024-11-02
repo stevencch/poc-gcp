cbcb5a5c4f900ccb96361a5b7ee553b18c131777
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-11-03 10:58:07 +1100

    add LoggerModule

--- apps/myjob1/src/app/app.module.ts
+++ apps/myjob1/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import appConfig from './app.config';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '@poc-gcp/logger';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
     }),
+    LoggerModule
   ],
   providers: [AppService],
 })
