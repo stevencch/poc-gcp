e728009437347b231ce03c15d0285db503ed1681
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-11-11 20:06:17 +1100

    test 10

--- terraform/modules/gcp/cloud_functions/main.tf
+++ terraform/modules/gcp/cloud_functions/main.tf
@@ -11,8 +11,8 @@ resource "google_project_iam_member" "function_sa_iam_binding" {
 
 data "archive_file" "default" {
   type        = "zip"
-  output_path = "../deploy/${var.object_name}.zip"
-  source_dir  = "../../../../dist/apps/${var.object_name}/"
+  output_path = "${path.module}/../deploy/${var.object_name}.zip"
+  source_file  = "${path.module}/variables.tf"
 }
 
 resource "google_storage_bucket_object" "function_zip" {
