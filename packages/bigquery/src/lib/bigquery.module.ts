b252364e8da58ccacff08690e4f81567a26f860d
Steven Cao <steven.cao@chemistwarehouse.com.au>
2024-11-22 13:19:19 +1100

    update4

--- packages/bigquery/src/lib/bigquery.module.ts
+++ packages/bigquery/src/lib/bigquery.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BigQueryService } from './bigquery.service';
+import { ConfigModule } from '@nestjs/config';
+import { BigQueryConfig } from './bigquery.config';
 
 @Module({
+  imports: [
+    ConfigModule.forFeature(BigQueryConfig)
+  ],
   providers: [BigQueryService],
   exports: [BigQueryService],
 })
