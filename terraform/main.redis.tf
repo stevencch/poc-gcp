68cc68a8968963da61b7e722e58d632281cff178
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-12 15:00:31 +1100

    fix 1

--- terraform/main.redis.tf
+++ terraform/main.redis.tf
@@ -1,11 +1,11 @@
 module "shared_redis" {
-    source             = "./modules/gcp/memory_store"
-    memory_size_gb     = 5
-    replica_count      = 2
-    gcp_vpc_network_id = data.google_compute_network.vpc_network.id
-  
-    service_name  = "common"
-    region_suffix = var.region_suffix[var.gcp_region]
-    // More info: Invalidating cloudfront through redis
-    description = "web-be"
-  }
+  source             = "./modules/gcp/memory_store"
+  memory_size_gb     = 5
+  replica_count      = 2
+  gcp_vpc_network_id = data.google_compute_network.vpc_network.id
+
+  service_name  = "common"
+  region_suffix = var.region_suffix[var.gcp_region]
+  // More info: Invalidating cloudfront through redis
+  description = "web-be"
+}
