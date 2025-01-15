a0d33b0f59b0ae796272f992090884b2518a1fb4
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-15 15:37:29 +1100

    update computer

--- terraform/main.compute.tf
+++ terraform/main.compute.tf
@@ -3,9 +3,9 @@ resource "google_service_account" "default" {
   display_name = "Custom SA for VM Instance"
 }
 
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "n2-standard-2"
+resource "google_compute_instance" "default_linux" {
+  name         = "my-instance1"
+  machine_type = "e2-micro"
   zone         = "us-central1-a"
 
   tags = ["foo", "bar"]
@@ -38,6 +38,45 @@ resource "google_compute_instance" "default" {
 
   metadata_startup_script = "echo hi > /test.txt"
 
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_instance" "default_win" {
+  name         = "my-instance2"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "sql-2016-standard-windows-2016-dc-v20241212"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     email  = google_service_account.default.email
