7ad431cdef7ca299fdfd268500e0e2913ac6a63d
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-17 21:38:04 +1100

    fix 6

--- terraform/main.computer_instance.tf
+++ terraform/main.computer_instance.tf
@@ -3,7 +3,7 @@ module "instance_simple" {
   project_id    = var.gcp_project_id
   region        = "us-central1"
   zone          = "us-central1-a"
-  subnetwork    = module.network_example.network_01_self_link.subnets.0.name
+  subnetwork    = module.network_example.network_01_subnets.0
   num_instances = 1
   service_account = {
     email  = google_service_account.default.email
@@ -16,7 +16,7 @@ module "instance_simple1" {
   project_id    = var.gcp_project_id
   region        = "us-central1"
   zone          = "us-central1-a"
-  subnetwork    = module.network_example.network_02_self_link.subnets.0.name
+  subnetwork    = module.network_example.network_02_subnets.0
   num_instances = 1
   service_account = {
     email  = google_service_account.default.email
