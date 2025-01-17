58667e62393812872218eaf76a4c9723b286538a
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-17 20:54:49 +1100

    test

--- terraform/main.computer_instance.tf
+++ terraform/main.computer_instance.tf
@@ -3,7 +3,20 @@ module "instance_simple" {
   project_id    = var.gcp_project_id
   region        = "us-central1"
   zone          = "us-central1-a"
-  subnetwork    = google_compute_subnetwork.main.self_link
+  subnetwork    = module.network_example.network_01_self_link
+  num_instances = 1
+  service_account = {
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+module "instance_simple1" {
+  source        = "./modules/compute_instance/simple"
+  project_id    = var.gcp_project_id
+  region        = "us-central1"
+  zone          = "us-central1-a"
+  subnetwork    = module.network_example.network_02_self_link
   num_instances = 1
   service_account = {
     email  = google_service_account.default.email
