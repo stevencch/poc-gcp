77a1b9250f21b4cceeca2e8f0e1a63b439a08761
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-17 21:54:31 +1100

    remove

--- terraform/main.computer_instance.tf
+++ terraform/main.computer_instance.tf
@@ -1,25 +1,25 @@
-module "instance_simple" {
-  source        = "./modules/compute_instance/simple"
-  project_id    = var.gcp_project_id
-  region        = "us-central1"
-  zone          = "us-central1-a"
-  subnetwork    = module.network_example.network_01_subnets.0
-  num_instances = 1
-  service_account = {
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
-}
+# module "instance_simple" {
+#   source        = "./modules/compute_instance/simple"
+#   project_id    = var.gcp_project_id
+#   region        = "us-central1"
+#   zone          = "us-central1-a"
+#   subnetwork    = module.network_example.network_01_subnets.0
+#   num_instances = 1
+#   service_account = {
+#     email  = google_service_account.default.email
+#     scopes = ["cloud-platform"]
+#   }
+# }
 
-module "instance_simple1" {
-  source        = "./modules/compute_instance/simple"
-  project_id    = var.gcp_project_id
-  region        = "us-central1"
-  zone          = "us-central1-a"
-  subnetwork    = module.network_example.network_02_subnets.0
-  num_instances = 1
-  service_account = {
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
-}
+# module "instance_simple1" {
+#   source        = "./modules/compute_instance/simple"
+#   project_id    = var.gcp_project_id
+#   region        = "us-central1"
+#   zone          = "us-central1-a"
+#   subnetwork    = module.network_example.network_02_subnets.0
+#   num_instances = 1
+#   service_account = {
+#     email  = google_service_account.default.email
+#     scopes = ["cloud-platform"]
+#   }
+# }
