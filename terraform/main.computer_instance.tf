50eb2d0525093449667a5c149c6b8ebcba14f867
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-17 18:02:23 +1100

    update zone

--- terraform/main.computer_instance.tf
+++ terraform/main.computer_instance.tf
@@ -2,6 +2,7 @@ module "instance_simple" {
   source        = "./modules/compute_instance/simple"
   project_id    = var.gcp_project_id
   region        = "us-central1"
+  zone          = "us-central1-a"
   subnetwork    = google_compute_subnetwork.main.self_link
   num_instances = 4
   service_account = {
