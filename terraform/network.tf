bf757ed211a0702238dee31b80d46d326c9ad768
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-19 08:40:18 +1100

    clean1

--- terraform/network.tf
+++ terraform/network.tf
@@ -20,26 +20,26 @@ resource "random_string" "suffix" {
   upper   = "false"
 }
 
-resource "google_compute_network" "main" {
-  project                 = var.gcp_project_id
-  name                    = "cft-vm-test-${random_string.suffix.result}"
-  auto_create_subnetworks = "false"
-}
+# resource "google_compute_network" "main" {
+#   project                 = var.gcp_project_id
+#   name                    = "cft-vm-test-${random_string.suffix.result}"
+#   auto_create_subnetworks = "true"
+# }
 
-resource "google_compute_subnetwork" "main" {
-  project       = var.gcp_project_id
-  region        = "us-east1"
-  name          = "cft-vm-test-${random_string.suffix.result}"
-  ip_cidr_range = "10.128.0.0/20"
-  network       = google_compute_network.main.self_link
-}
+# # resource "google_compute_subnetwork" "main" {
+# #   project       = var.gcp_project_id
+# #   region        = "us-east1"
+# #   name          = "cft-vm-test-${random_string.suffix.result}"
+# #   ip_cidr_range = "10.128.0.0/20"
+# #   network       = google_compute_network.main.self_link
+# # }
 
 
-resource "google_compute_network" "main1" {
-  project                 = var.gcp_project_id
-  name                    = "cft-vm-test1-${random_string.suffix.result}"
-  auto_create_subnetworks = "true"
-}
+# resource "google_compute_network" "main1" {
+#   project                 = var.gcp_project_id
+#   name                    = "cft-vm-test1-${random_string.suffix.result}"
+#   auto_create_subnetworks = "true"
+# }
 
 # resource "google_compute_subnetwork" "main1" {
 #   project       = var.gcp_project_id
