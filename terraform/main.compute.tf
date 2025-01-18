07284703e8add3a52a77d3e92a4846773466ecce
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-19 10:15:47 +1100

    add pc

--- terraform/main.compute.tf
+++ terraform/main.compute.tf
@@ -41,78 +41,78 @@ resource "google_compute_instance" "default_linux" {
   }
 }
 
-# resource "google_compute_instance" "default_linux1" {
-#   name         = "my-instance1-1"
-#   machine_type = "e2-micro"
-#   zone         = "us-east1-b"
+resource "google_compute_instance" "default_linux3-1" {
+  name         = "my-vpc-3-1"
+  machine_type = "e2-micro"
+  zone         = "us-east1-b"
 
-#   tags = ["foo", "bar"]
+  tags = ["foo", "bar"]
 
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-11"
-#       labels = {
-#         my_label = "value"
-#       }
-#     }
-#   }
-
-
-#   network_interface {
-#     network = google_compute_network.main.name
-
-#     access_config {
-#       // Ephemeral public IP
-#     }
-#   }
-
-#   metadata = {
-#     foo = "bar"
-#   }
-
-#   metadata_startup_script = "echo hi > /test.txt"
-
-#   service_account {
-#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-#     email  = google_service_account.default.email
-#     scopes = ["cloud-platform"]
-#   }
-# }
-
-# resource "google_compute_instance" "default_linux2" {
-#   name         = "my-instance1-2"
-#   machine_type = "e2-micro"
-#   zone         = "us-east1-b"
-
-#   tags = ["foo", "bar"]
-
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-11"
-#       labels = {
-#         my_label = "value"
-#       }
-#     }
-#   }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
 
 
-#   network_interface {
-#     network = google_compute_network.main1.name
+  network_interface {
+    network = google_compute_network.vpc-3.name
 
-#     access_config {
-#       // Ephemeral public IP
-#     }
-#   }
+    access_config {
+      // Ephemeral public IP
+    }
+  }
 
-#   metadata = {
-#     foo = "bar"
-#   }
+  metadata = {
+    foo = "bar"
+  }
 
-#   metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = "echo hi > /test.txt"
 
-#   service_account {
-#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-#     email  = google_service_account.default.email
-#     scopes = ["cloud-platform"]
-#   }
-# }
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
+resource "google_compute_instance" "default_linux4-1" {
+  name         = "my-vpc-4-1"
+  machine_type = "e2-micro"
+  zone         = "us-east1-b"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+
+  network_interface {
+    network = google_compute_network.vpc-4.name
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
