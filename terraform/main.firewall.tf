cdd30a29202cf28c280b632421f0171c5e1f78d0
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-18 23:06:42 +1100

    updte78

--- terraform/main.firewall.tf
+++ terraform/main.firewall.tf
@@ -1,9 +1,9 @@
-module "network_firewall_1" {
-  source     = "./modules/basic_firewall_rule"
-  name       = "my-firewall-rule"
-  project_id = var.gcp_project_id
-  network    = google_compute_network.main.self_link
-}
+# module "network_firewall_1" {
+#   source     = "./modules/basic_firewall_rule"
+#   name       = "my-firewall-rule"
+#   project_id = var.gcp_project_id
+#   network    = google_compute_network.main.self_link
+# }
 
 module "network_firewall_vpc1" {
   source     = "./modules/basic_firewall_rule"
