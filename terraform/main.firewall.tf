17c06f9064201efd6c4c3def8d14ccfd782918a2
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-19 11:15:00 +1100

    udpate

--- terraform/main.firewall.tf
+++ terraform/main.firewall.tf
@@ -7,14 +7,14 @@
 
 module "network_firewall_vpc3" {
   source     = "./modules/basic_firewall_ruleweb"
-  name       = "firewall-vpc3"
+  name       = "firewall-vpc3-web"
   project_id = var.gcp_project_id
   network    = google_compute_network.vpc-3.name
 }
 
 module "network_firewall_vpc3" {
   source     = "./modules/basic_firewall_rulessh"
-  name       = "firewall-vpc3"
+  name       = "firewall-vpc3-ssh"
   project_id = var.gcp_project_id
   network    = google_compute_network.vpc-3.name
 }
@@ -22,14 +22,14 @@ module "network_firewall_vpc3" {
 
 module "network_firewall_vpc4" {
   source     = "./modules/basic_firewall_ruleweb"
-  name       = "firewall-vpc4"
+  name       = "firewall-vpc4-web"
   project_id = var.gcp_project_id
   network    = google_compute_network.vpc-4.name
 }
 
 module "network_firewall_vpc4" {
   source     = "./modules/basic_firewall_rulessh"
-  name       = "firewall-vpc4"
+  name       = "firewall-vpc4-ssh"
   project_id = var.gcp_project_id
   network    = google_compute_network.vpc-4.name
 }
