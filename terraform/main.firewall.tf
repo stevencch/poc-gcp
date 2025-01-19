731850f25e1a5c67b21d3f83fafe4f71efd3dd26
Steven Cao <steven.cao@chemistwarehouse.com.au>
2025-01-19 11:22:37 +1100

    updtee323

--- terraform/main.firewall.tf
+++ terraform/main.firewall.tf
@@ -5,14 +5,14 @@
 # #   network    = google_compute_network.main.self_link
 # # }
 
-module "network_firewall_vpc3" {
+module "network_firewall_vpc3-web" {
   source     = "./modules/basic_firewall_ruleweb"
   name       = "firewall-vpc3-web"
   project_id = var.gcp_project_id
   network    = google_compute_network.vpc-3.name
 }
 
-module "network_firewall_vpc3" {
+module "network_firewall_vpc3-ssh" {
   source     = "./modules/basic_firewall_rulessh"
   name       = "firewall-vpc3-ssh"
   project_id = var.gcp_project_id
@@ -20,14 +20,14 @@ module "network_firewall_vpc3" {
 }
 
 
-module "network_firewall_vpc4" {
+module "network_firewall_vpc4-web" {
   source     = "./modules/basic_firewall_ruleweb"
   name       = "firewall-vpc4-web"
   project_id = var.gcp_project_id
   network    = google_compute_network.vpc-4.name
 }
 
-module "network_firewall_vpc4" {
+module "network_firewall_vpc4-ssh" {
   source     = "./modules/basic_firewall_rulessh"
   name       = "firewall-vpc4-ssh"
   project_id = var.gcp_project_id
